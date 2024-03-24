65610bf7eac20c3c1c8fdc45956d43c90a144b04
João Donato <joaovictor_gd@hotmail.com>
2024-03-24 16:12:52 -0300

    First commit

---
+++ src/main/java/br/com/donato/pedidolancamento/PedidoLancamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.donato.pedidolancamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoLancamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoLancamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/donato/pedidolancamento/PedidoLancamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.donato.pedidolancamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoLancamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
